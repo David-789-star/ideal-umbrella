29bada3b64c5cf4886cb1223b2e5b26989ddba19
David-789-star <segunoyeduntan@gmail.com>
2025-04-27 17:35:47 +0100

    Update lab_work1.py

--- lab_work1.py
+++ lab_work1.py
@@ -1,2 +1,9 @@
 # 1. Create a variable called car_name and assign the value 'lexus' to it
 car_name = "lexus"
+
+# 2. Create variables num_one and num_two with random integer values, then sum them
+num_one = 7  
+num_two = 12  
+sum_num = num_one + num_two
+print(sum_num)
+
