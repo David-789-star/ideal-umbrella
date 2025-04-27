33b36299ec9ca5b3efabde90b75190005633904b
David-789-star <segunoyeduntan@gmail.com>
2025-04-27 17:36:56 +0100

    Update lab_work1.py

--- lab_work1.py
+++ lab_work1.py
@@ -7,3 +7,8 @@ num_two = 12
 sum_num = num_one + num_two
 print(sum_num)
 
+# 3. Define three variables for the patient John Smith
+patient_name = "John Smith"
+patient_age = 20
+is_new_patient = True
+
