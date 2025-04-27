4ac5188f3104552c16bc92395623d0ef2b442cd6
David-789-star <segunoyeduntan@gmail.com>
2025-04-27 17:46:14 +0100

    Update Lab_work2.py

--- Lab_work2.py
+++ Lab_work2.py
@@ -1,5 +1,28 @@
-# 2. Create variables num_one and num_two with random integer values, then sum them
-num_one = 7  
-num_two = 12  
+#1 This program prints the famous poem "Twinkle, Twinkle, Little Star"
+print("Twinkle, twinkle, little star,")
+print("How I wonder what you are!")
+print("Up above the world so high,")
+print("Like a diamond in the sky.")
+print("Twinkle, twinkle, little star,")
+print("How I wonder what you are")
+
+#2 This program asks the user to input their first and last name
+first_name = input("Enter your First Name: ")  # Taking user's first name 
+last_name = input("Enter your Last Name: ")    # Taking user's last name 
+
+# Printing the full name together
+print("Your name is:", first_name, last_name)
+
+This program asks the user to input two numbers, adds them, and prints the result
+
+# taking the first number from the user and converting it to an integer
+num_one = int(input("Enter the first integer: "))
+
+# Taking the second number from the user and converting it to an integer
+num_two = int(input("Enter the second integer: "))
+
+# Adding the two numbers and storing the result in sum_num
 sum_num = num_one + num_two
-print(sum_num)
+
+# Printing the result
+print("The sum is:", sum_num)
