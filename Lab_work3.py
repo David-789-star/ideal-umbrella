ad406799845c87d4afa0fdc0c1974215cf4f33f0
David-789-star <segunoyeduntan@gmail.com>
2025-04-27 18:02:33 +0100

    Update Lab_work3.py

--- Lab_work3.py
+++ Lab_work3.py
@@ -1,5 +1,24 @@
 
-# 3. Define three variables for the patient John Smith
-patient_name = "John Smith"
-patient_age = 20
-is_new_patient = True
+# bag_name = "Gucci"
+
+# sum_num = 2 + 2
+
+# michael_cgpa = 5.0
+
+# is_boy = True
+
+# print(str(michael_cgpa) + bag_name)
+
+first_num = float(input("Enter First Number: "))
+second_num = float(input("Enter Second Number: "))
+operator = input("Enter Operator: ")
+
+
+if operator == "+":
+    sum_num = first_num + second_num
+    print(sum_num)
+
+first_num = 4
+second_num = 5
+operator = +
+sum_num = 9.0
