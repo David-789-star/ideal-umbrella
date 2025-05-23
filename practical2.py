faf8da0d887132df4db45ca23e5fe641eae144b4
David-789-star <segunoyeduntan@gmail.com>
2025-05-23 16:14:13 +0100

    Update practical2.py

--- practical2.py
+++ practical2.py
@@ -1,3 +1,5 @@
+#Let’s build a guess game. In this game we would have a secret number and then ask the user to Guess the secret number. If the user guesses it right before the 5th guess, the user wins and if he doesn’t the user loses the guess. 
+
 secret_number = 8
 guess_limit = 5
 guess_count = 0
