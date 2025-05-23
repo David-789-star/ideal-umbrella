37eb018c3632b333097688da6b06dca33eb486ac
David-789-star <segunoyeduntan@gmail.com>
2025-05-23 13:15:05 +0100

    Add files via upload

---
+++ practical3.py
@@ -0,0 +1,8 @@
+Fruit = ["apple", "Mango", "Banana"]
+print(Fruit)
+
+fruit = input("enter another fruit different from the listed one:  ")
+
+Fruit.append(fruit)
+
+print(f"The updated list : {Fruit}")
