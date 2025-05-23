63d75eadb8604ba5f887a04f2b2d7e87b66270e8
David-789-star <segunoyeduntan@gmail.com>
2025-05-23 15:57:39 +0100

    Update practical1.py

--- practical1.py
+++ practical1.py
@@ -1,7 +1,9 @@
-Number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+#using the loop write a program that print the sum of number in a list of 10 numbers
+
+Numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 Total = 0
 
-for num in Number:
+for num in Numbers:
 	Total = Total + num
 print(Total)
 
